dcde3f40956fa8f2827a5d088d00149d3da51d7a
RickFerry <rick_ferry1@hotmail.com>
2026-07-14 12:49:31 -0300

    refactor: rename application and test classes, add DMN files for discount and freight calculations

--- src/main/java/com/exemplo/camundademo/Application.java
+++ src/main/java/com/exemplo/camundademo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CamundaDemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CamundaDemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/com/exemplo/camundademo/delegate/VerificarScoreDelegate.java
+++ src/main/java/com/exemplo/camundademo/delegate/VerificarScoreDelegate.java
@@ -4,14 +4,17 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.min;
+
 @Component
 public class VerificarScoreDelegate implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution execution) {
         String cpf = (String) execution.getVariable("cpf");
-        int ultimosDigitos = Integer.parseInt(cpf.replaceAll("\\D", "").substring(9));
-        int score = Math.min(ultimosDigitos, 999);
+        int ultimosDigitos = parseInt(cpf.replaceAll("\\D", "").substring(9));
+        int score = min(ultimosDigitos, 999);
         execution.setVariable("score", score);
     }
 }
